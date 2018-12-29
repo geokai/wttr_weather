7afa623ad4d25c921416cb2138cbd61f94739938
George Kaimakis <geoptus@gmail.com>
2018-12-29 21:29:15 +0000

    Strip down and simplify to start again:
    The file was too complicated and getting messy so decided
    to strip it down to basics -
    no options parsing
    no functions
    - these will be re-developed

--- wttr_loop.sh
+++ wttr_loop.sh
@@ -5,56 +5,31 @@
 
 
 # usage function
-_usage () {
-    printf "  %b\n" ""\
-           ""\
-           ""
-}
+# _usage () {
+#     printf "  %b\n" ""\
+#                     "USAGE:"\
+#                     ""
+# }
 
 
-# handle fatal errors
-_fatal () {
-    return 2
-}
+# positional parameters: could also include a file with a list of places
+unset _locs
+_locs=($@)
+# _locs=(highgate+london falmouth+uk)
+# printf "%b\n" "item:" "${_locs[@]}"
+# printf "%b\n" "# of items: ${#_locs[@]}"
+# printf "$1\n"
+# printf "$2\n"
 
+_dir="$HOME/.weather/cache/"
+_interval=20    # seconds delay between wttr.in api calls
 
-# handle minor errors (warnings)
-_warning () {
-    return 1
-}
-
-
-# main function
-_run_main () {
-
-    # positional parameters: could also include a file with a list of places
-    _places="${@}"
-    _dir="~/.weather/cache/"
-    _interval=20    # seconds delay between wttr.in api calls
-    OPTIND=1
-
-
-    # parse option flags using getopts
-    while getopts ":hvV" opt; do
-        case $opt in
-            h) _usage && return 0;;
-            v) verbose=(( verbose+1 ))
-            V) set -x;;
-            *) _warning "";;
-        esac
-    done
- 
-    shift "$((OPTIND-1))"
-
-
-    # loop through the list of places with wttr function
-    _wttr_loop() {
-        for i in "${!_places[@]}"; do
-            wttr_cron.sh ~${_places[i]}?QM0 ${_dir}${_places[i]%\+*}.txt;
-            [ "${i}" -lt 1 ] && sleep ${_interval};
-        done
-    }
-}
+# loop through the list of places with wttr function
+for i in "${!_locs[@]}"; do
+    # echo "processing: ${_locs[i]}"
+    wttr_cron.sh ~${_locs[i]}?QM0 ${_dir}${_locs[i]%\+*}.txt;
+    [ "${i}" -lt $((${#_locs[@]}-1)) ] && sleep ${_interval};
+done
 
 # call the main function with all the positional parameters passed
-_run_main "${@}"
+# _run_main "$@"
